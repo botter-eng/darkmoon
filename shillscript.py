eb23f4a1955f559ae87bbf0ec5195ffead95f562
botter-eng <83904566+botter-eng@users.noreply.github.com>
2021-09-08 15:35:40 +0530

    Update shillscript.py

--- shillscript.py
+++ shillscript.py
@@ -13,22 +13,22 @@ phone = '+84924728421'  #telegram phonenr
 
 
 timer = 10 #delay in seconds, aka how long you want to wait before sending a msg again
-msgtosend = """Welcome to The DarkMoon Project
+msgtosend = """🌘Welcome to The DarkMoon Project🌘
 
-DarkMoon formed during a collision between the BTC Planet and another small planet SafeMoon! Rewarding holders with 10% in BTC to the DarkMoon Lovers and with the liquidity burnt to provide the holders with a happy sleep watching the Moon thinking about DarkMoon!
+🌘DarkMoon formed during a collision between the BTC Planet and another small planet SafeMoon! Rewarding holders with 10% in BTC to the DarkMoon Lovers and with the liquidity burnt to provide the holders with a happy sleep watching the Moon thinking about DarkMoon!
 
-We strive to create a Project on Binance Smart Chain which offers the following:
-Low Transaction Fee
-SAFU
-10% BTC Rewards to the Holders
+🌘We strive to create a Project on Binance Smart Chain which offers the following:
+✔️Low Transaction Fee
+✔️SAFU
+✔️10% BTC Rewards to the Holders
 
-SMART CONTRACTS : https://bscscan.com/token/0x92D9fbf6C7e3dfE6A57E1A553704D2a91BC97b73
+🌘SMART CONTRACTS : https://bscscan.com/token/0x92D9fbf6C7e3dfE6A57E1A553704D2a91BC97b73
 
-BUY HERE : https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x92D9fbf6C7e3dfE6A57E1A553704D2a91BC97b73
+🌘BUY HERE : https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x92D9fbf6C7e3dfE6A57E1A553704D2a91BC97b73
 
-LP BURN : https://bscscan.com/tx/0x0f1176fb427d551511526e52fa6a7b76fbbe9d2e052b439ab2da19eaff63b451
+🌘LP BURN : https://bscscan.com/tx/0x0f1176fb427d551511526e52fa6a7b76fbbe9d2e052b439ab2da19eaff63b451
 
-CHART : https://poocoin.app/tokens/0x92D9fbf6C7e3dfE6A57E1A553704D2a91BC97b73
+🌘CHART : https://poocoin.app/tokens/0x92D9fbf6C7e3dfE6A57E1A553704D2a91BC97b73
 
 Telegram Official : https://t.me/DarkMoon_project
 Website : https://darkmoonproject.space/
